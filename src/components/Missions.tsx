9fc1027abefa3d48854f3a6c384c701e1393ce06
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2024-07-10 11:09:06 -0300

    adiciona components e logica para renderizar os card das missions

---
+++ src/components/Missions.tsx
@@ -0,0 +1,22 @@
+import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
+
+function Missions() {
+  return (
+    <div data-testid="missions">
+      <Title headline="Missões" />
+      {missions.map((mission, index) => (
+        <MissionCard
+          key={ index }
+          name={ mission.name }
+          year={ mission.year }
+          country={ mission.country }
+          destination={ mission.destination }
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Missions;
