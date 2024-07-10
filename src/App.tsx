98b2f7ac5e5bfbedc9039afdf37b83c2e472f781
Bruno Pelegrino <pelegrinobruno13@gmail.com>
2024-07-10 09:21:37 -0300

    adiciona components Header e SolarSystem

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+
 function App() {
   return (
-    <>
-    </>
+    <div>
+      <Header />
+      <SolarSystem />
+    </div>
   );
 }
 
---
+++ src/components/SolarSystem.tsx
@@ -0,0 +1,5 @@
+function SolarSystem() {
+  return <div data-testid="solar-system" />;
+}
+
+export default SolarSystem;
